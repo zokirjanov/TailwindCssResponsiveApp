e1106bf8d64eef0cbe150d99cddfb816d958abef
zokirjanov <zokirjonovshohruxbek1016@gmail.com>
2024-06-21 00:33:58 +0500

    footer completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./build/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        'widescreen': {'raw': '(min-aspect-ratio: 3/2)'},
+        'tallscreen': {'raw': '(min-aspect-ratio: 1/2)'},
+        
+      }
+    },
   },
   plugins: [],
 }
